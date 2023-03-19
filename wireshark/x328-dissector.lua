24720e8d65107fb566bc8baa9933fb6a09f1f939
Lukas Sandström <lukas.sandstrom@chalmers.se>
2023-03-19 21:27:48 +0100

    Wireshark: Dissect node responses

--- wireshark/x328-dissector.lua
+++ wireshark/x328-dissector.lua
@@ -5,8 +5,13 @@ local x328_proto = Proto("x328", "X3.28 field bus")
 x328_proto.fields.address = ProtoField.string("x328.address", "Address")
 x328_proto.fields.parameter = ProtoField.string("x328.parameter", "Parameter")
 x328_proto.fields.value = ProtoField.string("x328.value", "Value")
+x328_proto.fields.bcc = ProtoField.uint8("x328.bcc", "BCC checksum")
+x328_proto.fields.response = ProtoField.string("x328.response", "Node response")
 
+local address_field = Field.new("x328.address")
 local param_field = Field.new("x328.parameter")
+local value_field = Field.new("x328.value")
+local response = Field.new("x328.response")
 
 function x328_proto.dissector(tvb, pinfo, tree)
 
@@ -15,13 +20,12 @@ function x328_proto.dissector(tvb, pinfo, tree)
     if pinfo.src == Address.ip("127.0.0.1") then
         dissect_master(tvb, pinfo, tree)
     else
-        pinfo.cols.info = "Reply:  "
+        disecct_node(tvb, pinfo, tree)
     end
 end
 
 function dissect_master(tvb, pinfo, tree)
-    pinfo.cols.info = "Master: "
-    tree:add(x328_proto.fields.address, tvb(1,4))
+    tree:add(x328_proto.fields.address, tvb(2,3))
     if tvb(5,1):uint() == 2 then -- write command
         local param = tree:add(x328_proto.fields.parameter, tvb(6,4))
         local value_len = 0
@@ -32,12 +36,29 @@ function dissect_master(tvb, pinfo, tree)
             end
         end
         tree:add(x328_proto.fields.value, tvb(10, value_len))
+        tree:add(x328_proto.fields.bcc, tvb(10 + value_len + 1, 1))
 
-        pinfo.cols.info = "Write " .. param_field()()
+
+        pinfo.cols.info = "Write addr " .. address_field()() .. " param " .. param_field()() .. " = " .. value_field()()
     else
         tree:add(x328_proto.fields.parameter, tvb(5,4))
-        pinfo.cols.info = "Query"
+        pinfo.cols.info = "Query addr " .. address_field()() .. " param " .. param_field()()
     end
+end
+
+function disecct_node(tvb, pinfo, tree)
+    pinfo.cols.info = "Reply: "
+
+    if tvb(0, 1):uint() == 6 then -- ACK
+        tree:add(x328_proto.fields.response,"ACK")
+    elseif tvb(0,1):uint() == 21 then -- NAK
+        tree:add(x328_proto.fields.response, "NAK")
+    else
+        tree:add(x328_proto.fields.parameter, tvb(1,4))
+        tree:add(x328_proto.fields.response, tvb(5, tvb:reported_len() - 5 - 2))
+    end
+
+    pinfo.cols.info = "Response: " .. response()()
 end
 
 prot_table = DissectorTable.get("udp.port")
